4a8bfb441fd13d6cd02fabfba03d7a74271c1d1c
William-Dev <williamlegall5@gmail.com>
2024-12-05 11:40:36 +0100

    New challenge

---
+++ index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
